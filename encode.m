4e1ac5ec9c9c30d2d709b8ceaea9fd6156368d6a
KoenGoe <koen.goe@hotmail.com>
2016-02-24 14:34:58 +0100

    first encode

---
+++ encode.m
@@ -0,0 +1,21 @@
+function out = encode(input, mu, phi)
+    out = zeros(length(input),1);
+    
+    prediction = 0;
+    stepsize = 1/8; %check scaling
+    prev_dequantized_sample = 0;
+    var = 0;
+    
+    for i = 1:length(input)
+        sample = input(i);
+        difference = sample - prediction;
+        quantized_difference = round(difference/stepsize);
+        out(i) = quantized_difference;
+        dequantized_difference = quantized_difference * stepsize;
+        var = (var * (i-1) + abs(quantized_difference))/i;
+        stepsize = phi*var;
+        dequantized_sample = dequantized_difference + prediction;
+        prediction = dequantized_sample - mu * prev_dequantized_sample;  
+        prev_dequantized_sample = dequantized_samples;
+    end
+end
