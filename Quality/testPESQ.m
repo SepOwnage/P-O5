7580a42671dc981a5b20411121d1a7863ebc4ad2
KoenGoe <koen.goe@hotmail.com>
2016-03-07 11:46:41 +0100

    temp to push to esat

--- Quality/testPESQ.m
+++ Quality/testPESQ.m
@@ -1,6 +1,6 @@
 results = zeros(21,2);
 for i = 1:20
-    buffer_lengths(2) = 30+i;
+    buffer_lengths(3) = 3*i;
     [~, results(i,1), results(i,2)] = ...
         run(depth, filter_lengths, Astops, scalings,...
         mus, phis, maxima, encode_version, buffer_lengths);
