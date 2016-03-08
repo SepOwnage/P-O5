ec209d004018fa2a9c46f4bebc6aa2752ff80175
Koen Goetschalckx <r0375967@pc-klas1-10.esat.kuleuven.be>
2016-03-08 08:07:56 +0100

    commit? what commit?

--- Quality/testPESQ.m
+++ Quality/testPESQ.m
@@ -1,7 +1,7 @@
 results = zeros(20,1);
 large_results = zeros(20*3*4*7,3*7+1);
 large_counter = 1;
-for sb = 1:7
+for sb = 5:7
     for loopiter = 1:4
         for i = 1:20
             buffer_lengths(sb) = 3*i;
@@ -38,12 +38,12 @@ for sb = 1:7
                 mus, phis, maxima, encode_version, buffer_lengths);
             %disp([i results(i,:)]);
             large_results(large_counter,:) =...
-                [buffer_lengths(1:4) phis(1:7) mus(1:7) results(i)];
+                [buffer_lengths(1:7) phis(1:7) mus(1:7) results(i)];
             large_counter = large_counter+1;
             
         end
         [score,imax] = max(results);
-        mus(sb) = 10+2*imax;
+        mus(sb) = imax*0.05;
         disp({'sb', sb, 'loopiter', loopiter, 'mus done', mus(sb), 'score', score});
     end
 end
