a296cf9d8ce97e043766b8f9bd36115bb82cec50
KoenGoe <koen.goe@hotmail.com>
2016-03-08 12:48:13 +0100

    best params for 5 (4 used) assymmetrical split as calculated by testPESQ

--- generate_some_params.m
+++ generate_some_params.m
@@ -1,9 +1,9 @@
 %an example to show the format of the parameters for run
 filter_lengths = {64,{{32,{{16}}},{32}}};
 Astops = {60,{{60,{{60}}},{60}}};
-mus = [1 1 1 1 1 ]* 0.19;
-phis = [.1881 .1881 .3352 .5860 0 ];  %negative make no sense
+mus = [.4 .0175 0.2 .325 1 ];
+phis = [.25 .35 .3352 .9 0 ];  %negative make no sense
 maxima = 2.^([5, 5, 4, 3, 1]-1)-1;
 scalings = {16,{{16,{{16}}},{16}}};
 encode_version = 1;
-buffer_lengths = [1 1 1 1 1]*8;
+buffer_lengths = [10 8 16 13 1];
