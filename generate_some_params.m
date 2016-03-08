831c0cea19354d61637bcffcc97db2e38367c76f
KoenGoe <koen.goe@hotmail.com>
2016-03-08 14:01:47 +0100

    again, but for short test file (better overall results)

--- generate_some_params.m
+++ generate_some_params.m
@@ -1,9 +1,9 @@
 %an example to show the format of the parameters for run
 filter_lengths = {64,{{32,{{16}}},{32}}};
 Astops = {60,{{60,{{60}}},{60}}};
-mus = [.4 .0175 0.2 .325 1 ];
-phis = [.25 .35 .3352 .9 0 ];  %negative make no sense
+mus = [.125 .125 0.025 .275 1 ];
+phis = [.2 .2 .5 .55 0 ];  %negative make no sense
 maxima = 2.^([5, 5, 4, 3, 1]-1)-1;
 scalings = {16,{{16,{{16}}},{16}}};
 encode_version = 1;
-buffer_lengths = [10 8 16 13 1];
+buffer_lengths = [7 7 9 2 1];
