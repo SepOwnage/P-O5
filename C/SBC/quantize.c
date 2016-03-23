05aec24632ff0888414a3ee63a95fb3ff0a706f3
SepOwnage <seppeiven@gmail.com>
2016-03-23 09:11:28 +0100

    Quantize syntax-error vrij

--- C/SBC/quantize.c
+++ C/SBC/quantize.c
@@ -1,31 +1,22 @@
 #include "globals.h"
+#include "quantize.h"
 
-struct parameters{ //This is characterised by the subband
-	unsigned short phi; //TODO check all these types
-	unsigned short mu;
-	unsigned short buffer_length;
-}
-
-struct start_values{ //This is characterised by previous samples
-	signed short initial_prediction;
-	unsigned short initial_stepsize;
-	signed short prev_dequantized_sample;
-	short *buffer; //* since this struct doesn't know buffer_length: Maybe this is cumbersome and both structs should be combined into one struct
-}
-
-short *quantize(short samples[BUFFERSIZE/8],struct parameters *params, struct start_values *values){
+ //Als wij ooit clippen/overflowen, gaat de fout zich voortplanten TODO
+void quantize(short quantized_differences[BUFFERSIZE/8], short samples[BUFFERSIZE/8],struct parameters *params, struct start_values *values){
 	// Variable declaration & initialisation
 	unsigned short phi = params->phi;
 	unsigned short mu = params->mu;
 	unsigned short buffer_length = params->buffer_length;
 	signed short difference; //Check if has to be short
 	short dequantized_difference; //Check if has to be short
-	short quantized_differences[BUFFERSIZE/8]; ///Check if has to be short
 	signed short prediction = values->initial_prediction; //Check if has to be short (due to mu and subtraction)
 	unsigned short stepsize = values->initial_stepsize; //Check if has to be short
+	signed short dequantized_sample;
 	signed short prev_dequantized_sample = values->prev_dequantized_sample;
-	unsigned short buffer[buffer_length] = values->buffer[buffer_length]; //Same type as quantized_differences but unsigned. TODO: This initialisation will not work, but it is supposed to read buffer_length amount of values, starting at 
-																			//*buffer from the struct, and then fill buffer[buffer_length] with these values
+	unsigned int buffersum;
+	unsigned short buffer[buffer_length]; //Same type as quantized_differences but unsigned
+	copy(buffer,values->buffer,params->buffer_length);
+	
 	
 	// Loop
 	for(unsigned char i = 0; i < BUFFERSIZE/8; i++){
@@ -34,24 +25,37 @@ short *quantize(short samples[BUFFERSIZE/8],struct parameters *params, struct st
 		quantized_differences[i] = (short) difference/stepsize; //C should round this by itself
 		dequantized_difference = quantized_differences[i] * stepsize;
 
-		buffersum = buffersum - buffer[mod(i,buffer_length)]+abs(dequantized_difference); //The +1 in matlab code should only be there because matlab indexing starts at 1?
-		buffer[mod(i,buffer_length)] = abs(dequantized_difference); //The +1 in matlab code should only be there because matlab indexing starts at 1?
-		stepsize = max(phi*buffersum/buffer_length,1);
+		buffersum = buffersum - buffer[i % buffer_length]+absvalue(dequantized_difference); //The +1 in matlab code should only be there because matlab indexing starts at 1?
+		buffer[i % buffer_length] = absvalue(dequantized_difference); //The +1 in matlab code should only be there because matlab indexing starts at 1?
+		stepsize = max(phi*buffersum/buffer_length,1); //Mag stepsize wel short zijn?
 		dequantized_sample = dequantized_difference + prediction;
-		prediction = dequantized_sample - mu * prev_dequantized_sample
-		prev_dequantized_sample = dequantized_sample
+		prediction = dequantized_sample - mu * prev_dequantized_sample;
+		prev_dequantized_sample = dequantized_sample;
 
 	}
-
-	return quantized_differences;
 	
 }
 
-unsigned char mod(unsigned char number, unsigned short modulo){ //Type should be same as buffer
-	while(number >= modulo){
-		number -= modulo;
+void copy(unsigned short *destination, unsigned short *values, unsigned char length){
+	for(unsigned char i = 0; i < length; i++){
+		destination[i] = values[i];
 	}
-	return number;
+}
+
+unsigned short absvalue(short value){
+
+	if(value < 0){
+		value = value * -1; //Will converting to short give a runtime error?
+	}
+	return value;
+}
+
+unsigned short max(unsigned int a,unsigned char b){ //TODO check type of a: phi*buffersum/buffer_length
+	if (a > b){
+		return a;
+	}else{
+		return b;
+	}
 }
 
 
---
+++ C/SBC/quantize.h
@@ -0,0 +1,17 @@
+struct parameters{ //This is characterised by the subband
+	unsigned short phi; //TODO check all these types
+	unsigned short mu;
+	unsigned char buffer_length;
+};
+
+struct start_values{ //This is characterised by previous samples
+	signed short initial_prediction;
+	unsigned short initial_stepsize;
+	signed short prev_dequantized_sample;
+	short *buffer; //* since this struct doesn't know buffer_length: Maybe this is cumbersome and both structs should be combined into one struct
+};
+
+void quantize(short quantized_differences[BUFFERSIZE/8], short samples[BUFFERSIZE/8],struct parameters *params, struct start_values *values);
+void copy(unsigned short *destination, unsigned short *values, unsigned char length);
+unsigned short absvalue(short value);
+unsigned short max(unsigned int a,unsigned char b);
