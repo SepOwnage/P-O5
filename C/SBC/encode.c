cf6655508fde3c2649208d1277d7eff94e60c739
KoenGoe <koen.goe@hotmail.com>
2016-03-27 14:05:51 +0200

    comments

--- C/SBC/encode.c
+++ C/SBC/encode.c
@@ -1,21 +1,18 @@
 #include "encode.h"
 #include <stdio.h>
 void addBufferToHistory(short buffer[BUFFERSIZE], struct chunk *historyChunk){
+	/*
+	Takes an input buffer 'buffer' and copy the values in it to the corresponding arrays in historyChunk
+	at position historyChunk -> position1.  historyChunk->position1 is moved to position after copied values.
+	Note that if the end of the arrays in historyChunk is reached, writing will continue at the beginning of the array.
+	(FIFO like structure);
+	*/
 	int i;
 	for(i=0;i<BUFFERSIZE/4;i++){
-	    //printf("%x \n", historyChunk->leftEven);
-	    //printf("%x %d ", ((historyChunk->leftEven) + (historyChunk->position1)), *((historyChunk->leftEven) + (historyChunk->position1)));
-		*((historyChunk->leftEven) + (historyChunk->position1)) = buffer[4*i];
-		//printf(" %d \n", *((historyChunk->leftEven) + (historyChunk->position1)));
-		//printf("%x %d ", ((historyChunk->leftOdd) + (historyChunk->position1)), *((historyChunk->leftOdd) + (historyChunk->position1)));
+		*((historyChunk->leftEven) + (historyChunk->position1)) = buffer[4*i];		
 		*((historyChunk->rightEven) + (historyChunk->position1)) = buffer[4*i+1];
-		//printf(" %d \n", *((historyChunk->leftOdd) + (historyChunk->position1)));
-		//printf("%x %d ", ((historyChunk->rightEven) + (historyChunk->position1)), *((historyChunk->rightEven) + (historyChunk->position1)));
 		*((historyChunk->leftOdd) + (historyChunk->position1)) = buffer[4*i+2];
-		//printf(" %d \n", *((historyChunk->rightEven) + (historyChunk->position1)));
-		//printf("%x %d ", ((historyChunk->rightOdd) + (historyChunk->position1)), *((historyChunk->rightOdd) + (historyChunk->position1)));
 		*((historyChunk->rightOdd) + (historyChunk->position1)) = buffer[4*i+3];
-		//printf(" %d \n", *((historyChunk->rightOdd) + (historyChunk->position1)));
 		(historyChunk->position1) = ((historyChunk->position1) +1) % (BUFFERSIZE/4 + LENGTH_FILTER1_HALF);
 	}
 }
@@ -24,28 +21,41 @@ void convolve(short *input, short *reversedFilter,
 		unsigned short inputOffset, unsigned short inputL,
 		unsigned short filterL, 
 		short *output, unsigned short outputOffset, 
-		unsigned short outputLength, unsigned short amountToShift){ //must be able to work in place? => works in place when output == input
-	//TODO: bitshift does floor instead of to zero
-	unsigned short i,j;
-	long long result;
-	unsigned short stop = inputL-filterL; // 10
-	for(j=0; j < stop; j++){ // 0 tem 9
+		unsigned short outputLength, unsigned short amountToShift){
+	/*Convolves two signals:
+	input: start position of the array holding the input signal
+	reversedFilter: start position of the array holding the filter in reversed order!
+	inputOffset: where in the array holding the input signal, the input signal starts
+	inputL: the length of the array holding the input signal (reading after this length is
+			done by reading the start of the array, i.e. modulo on indeces; FIFO like structure)
+	filterL: the length of the filter;
+	output: start position of the array in which to write the output
+	outputOffset: similar to inputOffset
+	outputLength: similar to inputL
+	amountToShift: output is downscaled by 2^amountToShift
+
+	Note that this function can work in place if output is input
+	*/	
+	unsigned short i,j; //bookkeepings
+	long long result; //holds the temporary unscaled result
+	unsigned short stop = inputL-filterL; // the amount of samples that can be calculated
+
+	for(j=0; j < stop; j++){ 
 		result = 0;
 		unsigned short smallstop = filterL;
-		for(i = 0; i<smallstop; i++){ // 1 tem 40   ....   10 tem 49
-		    //printf("j:%d i:%d bufsample: %p ,  filtersample: %p \n",j,i,(void *)  (input+(i+inputOffset+j+1)%inputL),(void *) (reversedFilter+i));
-		    //printf("bufsample: %d ,  filtersample: %d \n", *(input+(i+inputOffset)%inputL),*(reversedFilter+i));
-		    //printf("%d,%d|",i, (i+inputOffset)%inputL);
-			result += (*(input+(i+inputOffset+j+1)%inputL)) * (*(reversedFilter+i));    //laatste  = input+i = input+filterL+j = input+filterL+inputL-filterL
+		for(i = 0; i<smallstop; i++){
+			//read data (use modulo to wrap around to beginning of array if necessary), multiply
+			//with filter coeff and add to temporary result
+			result += (*(input+(i+inputOffset+j+1)%inputL)) * (*(reversedFilter+i));
 		}
+		//scale the result, clip it to short range if necessary (so it positives don't become
+		//negatives and vice versa when converting to short).
 		result = result/(1<<amountToShift);
 		if (result > 32767)
 			result = 32767;
 		else if (result < -32768)
 			result = -32767;
-		else if (result > 32000)
-			printf("|");
-		//printf("outsample %p",(void *) ( output + (j+outputOffset)%outputLength));
+		//write away output
 		*(output + (j+outputOffset)%outputLength) = (short)(result);
 	} 
 }
@@ -53,20 +63,42 @@ void convolve(short *input, short *reversedFilter,
 void combine(short *upper, short *lower, short *out_low, short *out_high,
 			 unsigned short length, unsigned short start, unsigned short arrayLength,
 			 signed short *combineTransfer){
-			//can work in place with in and outputs the same
+	/*
+	combines upper and lower signals in QMF scheme to low and high frequency signals.
+		low_frequency = upper + lower
+		high_frequency = upper - lower
+	upper: start of array holding the upper signal
+	lower: start of array holding the lower signal
+	out_low: start of array in which low frequency band should be written
+	out_high: start of array in which high frequency band should be written
+	length: the amount of samples to process
+	start: where in the arrays the current data starts and should be written
+	arrayLength: the length of previous array. Writing or reading after this length 
+			corresponds to writing reading in the beginning of them (modulo on index, FIFO like)
+	combineTransfer: holds the last value of the lower signal from the previous call. This is to 
+					 implement the one sample delay on the lower signal. The last value of the
+					 current lower signal will be saved here  (just initialize the value at
+					 combineTransfer to 0 and then keep passing the same combineTransfer).
+	Can work in place
+	*/
 	short i;
+	//temporary value holding the value to write in the high frequency output
 	short highToWrite;
+	//temporary values holding a sample from the upper and lower signal
 	int uppervalue, lowervalue;
+	//the current position in the arrays
 	short position;
-	//first one using transfer
+	//first one using combineTransfer
 	position = start % arrayLength;
 	uppervalue = *(upper + position);
 	lowervalue = *combineTransfer;	
 	*(out_low + position) = (uppervalue + lowervalue) / 2;
 	highToWrite = (uppervalue - lowervalue) / 2;
+	//read it already, since it might be overwritten when working in place
 	lowervalue = *(lower + position);
 	*(out_high + position) = highToWrite;
-
+	
+	//loop for other values
 	for(i=1;i<length;i++){
 		position = (start + i) % arrayLength;
 		uppervalue = *(upper + position);
@@ -75,10 +107,16 @@ void combine(short *upper, short *lower, short *out_low, short *out_high,
 		lowervalue = *(lower + position);
 		*(out_high + position) = highToWrite;
 	}
+	//save the last value of the lower signal for the next call
 	*combineTransfer = lowervalue;
 }
 
 void copyToLowerLayer(struct chunk *historyChunk){
+	/*
+	Takes the samples from depth 1 and writes them to appropriate arrays of depth 2
+	Using the FIFO structures to wrap around the endings as always. position1 is not changed,
+	position2 is set after the newly written data
+	*/
 	int i;
 	for(i=0;i < BUFFERSIZE/8;i++){   //   /8 ?/
 		*(historyChunk->leftLowEven + historyChunk->position2) = 
@@ -106,6 +144,8 @@ void copyToLowerLayer(struct chunk *historyChunk){
 }
 
 void encode(short buffer[BUFFERSIZE],struct chunk *historyChunk){
+	//encodes the new input samples, stored in buffer. Right now, only conversion to subbands is done.
+
 	//copy new buffer to chunk of history, overwriting oldest data that no longer matters
 	addBufferToHistory(buffer, historyChunk);
 	
--- C/SBC/main.c
+++ C/SBC/main.c
@@ -13,7 +13,9 @@ static short buffer_lengths[4] = BUFFER_LENGTHS;
 
 struct wavpcm_input input;
 struct wavpcm_output output_LL, output_LH, output_HL, output_HH, output_L, output_H;
+//the buffer in which input is read
 short buffer[BUFFERSIZE];
+//for testing purposes, we'll write every subband to a file => output buffer for each subband
 short reconstructedBuffer_LL[BUFFERSIZE / 4];
 short reconstructedBuffer_LH[BUFFERSIZE / 4];
 short reconstructedBuffer_HL[BUFFERSIZE / 4];
@@ -21,13 +23,15 @@ short reconstructedBuffer_HH[BUFFERSIZE / 4];
 short reconstructedBuffer_L[BUFFERSIZE / 2];
 short reconstructedBuffer_H[BUFFERSIZE / 2];
 
+//The chunk to pass between calls of encode  (contains previous samples)
 struct chunk theChunk;
+//Bookkeeping
 int bufPos, bufIndex, read;
 
 /* This is the function that is called when the program starts. */
 int main(int argc, char *argv[])
 {
-	/* Variable declarations. */
+	//Variable initialization (to 0)
 
 	memset(&theChunk, 0, sizeof(struct chunk));
 
@@ -48,7 +52,7 @@ int main(int argc, char *argv[])
 
 	/* First open input file and parse header, */
 	wavpcm_input_open(&input);
-	/* and then use this same header configuration for the output file */
+	/* and then copy this same header configuration for the output file */
 	wavpcm_output_copy_settings(&input, &output_LL);
 	(output_LL.samplingRate) /= 4;
 	wavpcm_output_copy_settings(&input, &output_LH);
@@ -79,9 +83,7 @@ int main(int argc, char *argv[])
 		/* transform buffer */
 		encode(buffer, &theChunk);
 
-		/* if required, dump compressed output */
-
-		/* inverse transform buffer */
+		//fill all output buffers...
 		for (bufIndex = 0; bufIndex < BUFFERSIZE / 8; bufIndex++) {
 			reconstructedBuffer_LL[2 * bufIndex] = theChunk.leftLowEven[(bufIndex + theChunk.position2) % (BUFFERSIZE / 8 + LENGTH_FILTER2_HALF)];
 			reconstructedBuffer_LL[2 * bufIndex + 1] = theChunk.rightLowEven[(bufIndex + theChunk.position2) % (BUFFERSIZE / 8 + LENGTH_FILTER2_HALF)];
@@ -98,7 +100,7 @@ int main(int argc, char *argv[])
 			reconstructedBuffer_H[2 * bufIndex] = theChunk.leftOdd[(bufIndex + theChunk.position1) % (BUFFERSIZE / 4 + LENGTH_FILTER1_HALF)];
 			reconstructedBuffer_H[2 * bufIndex + 1] = theChunk.rightOdd[(bufIndex + theChunk.position1) % (BUFFERSIZE / 4 + LENGTH_FILTER1_HALF)];
 		}
-		/* dump reconstructed output */
+		//... and dump them to their files
 		wavpcm_output_write(&output_LL, reconstructedBuffer_LL, read / 4);
 		wavpcm_output_write(&output_LH, reconstructedBuffer_LH, read / 4);
 		wavpcm_output_write(&output_HL, reconstructedBuffer_HL, read / 4);
