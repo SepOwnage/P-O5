5b920d6d87c96aeb35d8240fbc45a7f7d8002499
KoenGoe <koen.goe@hotmail.com>
2016-04-19 09:30:17 +0200

    fixes

--- C/SBC/main.c
+++ C/SBC/main.c
@@ -15,24 +15,37 @@
 #include "crypto/CCM.h"
 #include "crypto/cryptoMain.h"
 
-#define NB_OF_SMALL_BUFFERS_IN_LARGE 80
+#define NB_OF_SMALL_BUFFERS_IN_LARGE 40
 
 struct parameters LowLowParams = { 5144, 19660, 10, 15 };
-unsigned short LowLowValuesLeft[15];
-struct start_values LowLowStartValuesLeft = { 0, 1, 0, 0, 0, LowLowValuesLeft };
+unsigned short LowLowValuesLeftQuantize[15];
+struct start_values LowLowStartValuesLeftQuantize = { 0, 1, 0, 0, 0, LowLowValuesLeftQuantize };
+unsigned short LowLowValuesLeftDequantize[15];
+struct start_values LowLowStartValuesLeftDequantize = { 0, 1, 0, 0, 0, LowLowValuesLeftDequantize };
 struct parameters LowHighParams = { 16384, 328, 10, 7 };
-unsigned short LowHighValuesLeft[15];
-struct start_values LowHighStartValuesLeft = { 0, 1, 0, 0, 0, LowHighValuesLeft };
-struct parameters HighParams = { 29490, 31129, 10, 3 };
-unsigned short HighValuesLeft[15];
-struct start_values HighStartValuesLeft = { 0, 1, 0, 0, 0, HighValuesLeft };
+unsigned short LowHighValuesLeftQuantize[15];
+unsigned short LowHighValuesLeftDequantize[15];
+struct start_values LowHighStartValuesLeftQuantize = { 0, 1, 0, 0, 0, LowHighValuesLeftQuantize };
+struct start_values LowHighStartValuesLeftDequantize = { 0, 1, 0, 0, 0, LowHighValuesLeftDequantize };
 
-unsigned short LowLowValuesRight[15];
-struct start_values LowLowStartValuesRight = { 0, 1, 0, 0, 0, LowLowValuesRight };
-unsigned short LowHighValuesRight[15];
-struct start_values LowHighStartValuesRight = { 0, 1, 0, 0, 0, LowHighValuesRight };
-unsigned short HighValuesRight[15];
-struct start_values HighStartValuesRight = { 0, 1, 0, 0, 0, HighValuesRight };
+struct parameters HighParams = { 29490, 31129, 10, 3 };
+unsigned short HighValuesLeftQuantize[15];
+unsigned short HighValuesLeftDequantize[15];
+struct start_values HighStartValuesLeftQuantize = { 0, 1, 0, 0, 0, HighValuesLeftQuantize };
+struct start_values HighStartValuesLeftDequantize = { 0, 1, 0, 0, 0, HighValuesLeftDequantize };
+
+unsigned short LowLowValuesRightQuantize[15];
+unsigned short LowLowValuesRightDequantize[15];
+struct start_values LowLowStartValuesRightQuantize = { 0, 1, 0, 0, 0, LowLowValuesRightQuantize };
+struct start_values LowLowStartValuesRightDequantize = { 0, 1, 0, 0, 0, LowLowValuesRightDequantize };
+unsigned short LowHighValuesRightQuantize[15];
+unsigned short LowHighValuesRightDequantize[15];
+struct start_values LowHighStartValuesRightQuantize = { 0, 1, 0, 0, 0, LowHighValuesRightQuantize };
+struct start_values LowHighStartValuesRightDequantize = { 0, 1, 0, 0, 0, LowHighValuesRightDequantize };
+unsigned short HighValuesRightQuantize[15];
+unsigned short HighValuesRightDequantize[15];
+struct start_values HighStartValuesRightQuantize = { 0, 1, 0, 0, 0, HighValuesRightQuantize };
+struct start_values HighStartValuesRightDequantize = { 0, 1, 0, 0, 0, HighValuesRightDequantize };
 
 struct chunk historyChunkAnalysis, historyChunkSynthesis;
 unsigned char largeCryptoBuffer[15 * NB_OF_SMALL_BUFFERS_IN_LARGE + 45];  //TODO reken die 30 juist uit
@@ -51,6 +64,8 @@ int main(int argc, char *argv[]){
 	message_ctx ciphermessage;
 	RSA_ctx RSA_ctx_master, RSA_ctx_slave;
 	ENC_ctx ENC_ctx_master, ENC_ctx_slave;
+
+	unsigned char test[2] = { 0,0 };
 
 	memset(&input, 0, sizeof(struct wavpcm_input));
 	input.resource = INPUTWAVFILE;
@@ -95,27 +110,27 @@ int main(int argc, char *argv[]){
 			quantize((short *) (largeCryptoBuffer + placeInLargeBuffer),
 				historyChunkAnalysis.leftLowEven, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &LowLowParams, &LowLowStartValuesLeft);
-			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer + 5),
+				5, &LowLowParams, &LowLowStartValuesLeftQuantize);
+			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer) + 5,
 				historyChunkAnalysis.rightLowEven, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &LowLowParams, &LowLowStartValuesRight);
-			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer + 10),
+				5, &LowLowParams, &LowLowStartValuesRightQuantize);
+			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer) + 10,
 				historyChunkAnalysis.leftLowOdd, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &LowHighParams, &LowHighStartValuesLeft);
-			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer + 15),
+				5, &LowHighParams, &LowHighStartValuesLeftQuantize);
+			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer) + 15,
 				historyChunkAnalysis.rightLowOdd, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &LowHighParams, &LowHighStartValuesRight);
-			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer + 20),
+				5, &LowHighParams, &LowHighStartValuesRightQuantize);
+			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer) + 20,
 				historyChunkAnalysis.leftHighEven, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &HighParams, &HighStartValuesLeft);
-			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer + 25),
+				5, &HighParams, &HighStartValuesLeftQuantize);
+			quantize((short *)(largeCryptoBuffer + placeInLargeBuffer) + 25,
 				historyChunkAnalysis.rightHighEven, historyChunkAnalysis.position2,
 				BUFFERSIZE_DIV8 + LENGTH_FILTER2_HALF,
-				5, &HighParams, &HighStartValuesRight);
+				5, &HighParams, &HighStartValuesRightQuantize);
 
 			compress30Samples((short *) (largeCryptoBuffer + placeInLargeBuffer));
 
@@ -123,11 +138,11 @@ int main(int argc, char *argv[]){
 		}
 
 		//encrypt
-		sendData(&ENC_ctx_master, largeCryptoBuffer, sizeof(largeCryptoBuffer), &ciphermessage);
+		//sendData(&ENC_ctx_master, largeCryptoBuffer, sizeof(largeCryptoBuffer), &ciphermessage);
 		//channel
 
 		//decrypt
-		readData(&ENC_ctx_slave, &ciphermessage, largeCryptoBuffer, &decrypt_size);
+		//readData(&ENC_ctx_slave, &ciphermessage, largeCryptoBuffer, &decrypt_size);
 
 		//undo speech part
 		placeInLargeBuffer = 0;
@@ -136,12 +151,12 @@ int main(int argc, char *argv[]){
 			decompress30samples((largeCryptoBuffer + placeInLargeBuffer), wavbuffer);
 
 			//TODO put in one (inline?) function
-			dequantize(wavbuffer, wavbuffer, 5, &LowLowParams, &LowLowStartValuesLeft);
-			dequantize(wavbuffer + 5 , wavbuffer + 5, 5, &LowLowParams, &LowLowStartValuesRight);
-			dequantize(wavbuffer + 10, wavbuffer + 10, 5, &LowHighParams, &LowHighStartValuesLeft);
-			dequantize(wavbuffer + 15, wavbuffer + 15, 5, &LowHighParams, &LowHighStartValuesRight);
-			dequantize(wavbuffer + 20, wavbuffer + 20, 5, &HighParams, &HighStartValuesLeft);
-			dequantize(wavbuffer + 25, wavbuffer + 25, 5, &HighParams, &HighStartValuesRight);
+			dequantize(wavbuffer, wavbuffer, 5, &LowLowParams, &LowLowStartValuesLeftDequantize);
+			dequantize(wavbuffer + 5 , wavbuffer + 5, 5, &LowLowParams, &LowLowStartValuesRightDequantize);
+			dequantize(wavbuffer + 10, wavbuffer + 10, 5, &LowHighParams, &LowHighStartValuesLeftDequantize);
+			dequantize(wavbuffer + 15, wavbuffer + 15, 5, &LowHighParams, &LowHighStartValuesRightDequantize);
+			dequantize(wavbuffer + 20, wavbuffer + 20, 5, &HighParams, &HighStartValuesLeftDequantize);
+			dequantize(wavbuffer + 25, wavbuffer + 25, 5, &HighParams, &HighStartValuesRightDequantize);
 
 			synthesis(wavbuffer,
 				wavbuffer, wavbuffer + 10, wavbuffer + 20, wavbuffer + 5, wavbuffer + 15, wavbuffer + 25,
@@ -151,4 +166,6 @@ int main(int argc, char *argv[]){
 			placeInLargeBuffer += 15;
 		}
 	}
+	wavpcm_input_close(&input);
+	wavpcm_output_close(&output);
 }
