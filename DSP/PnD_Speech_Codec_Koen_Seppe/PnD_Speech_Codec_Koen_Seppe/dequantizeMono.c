2f8e4b4ca6ec0183901a695a5094843bf4a3ab95
KoenGoe <koen.goe@hotmail.com>
2016-05-14 20:08:06 +0200

    includes wa cleaner

--- DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/dequantizeMono.c
+++ DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/dequantizeMono.c
@@ -1,7 +1,6 @@
 #include "globals.h"
-#include "quantizeTogether.h"
+#include "quantizeStructs.h"
 #include "dequantizeMono.h"
-#include <stdio.h>
 
 void dequantizeMono(short *dequantized_samples_left, short *samples_left,
                 unsigned char nb_samples_to_do, struct parameters *params, struct start_values *values_left,short *dequantized_samples_right, short *samples_right,
