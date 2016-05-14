6527a2eda761270586111c7bbcd65170d1116119
SepOwnage <seppeiven@hotmail.com>
2016-05-14 17:54:25 +0200

    hotfix

--- DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/quantizeMono.c
+++ DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/quantizeMono.c
@@ -2,7 +2,7 @@
 #include "quantize.h"
 #include <stdio.h>
 
-void quantizeTogether(short * restrict quantized_differences_left, short * restrict start_of_samples_array_left,
+void quantizeMono(short * restrict quantized_differences_left, short * restrict start_of_samples_array_left,
 	unsigned char start_position_in_samples_array_left, unsigned char length_of_samples_array_left,
 	unsigned char nb_samples_to_do, struct parameters *params_left, struct start_values *values_left,
 	short * restrict quantized_differences_right, short * restrict start_of_samples_array_right,
