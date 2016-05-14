bda7d0245e8c20539a25c42016054ef698280337
KoenGoe <koen.goe@hotmail.com>
2016-05-14 19:53:08 +0200

    ook in normale quantize en dequantize zo gezet

--- DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/dequantize.h
+++ DSP/PnD_Speech_Codec_Koen_Seppe/PnD_Speech_Codec_Koen_Seppe/dequantize.h
@@ -1,3 +1,4 @@
+#include "quantizeStructs.h"
 void dequantize(short *dequantized_differences, short *samples,
                 unsigned char nb_samples_to_do, struct parameters *params, struct start_values *values);
 
